901a1f3c932c9749e73afb92f4c136de761767bd
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2023-01-02 16:39:25 +0000

    Update UserRepository.php

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -181,9 +181,9 @@ class UserRepository
     public function deleteJobExperience($USER_NIF, $USER_PASS, $USER_EMAIL, $JOB_EXPERIENCE_ID) 
     {
 
-        DB::beginTransaction();
-        
-        $user = DB::select("SELECT * FROM API_USER_JOB_EXPERIENCE_DELETE($USER_NIF, '$USER_PASS', '$USER_EMAIL', '$JOB_EXPERIENCE_ID')"); 
+        $user = DB::executeProcedure('API_USER_JOB_EXPERIENCE_DELETE', [$USER_NIF, $USER_PASS, $USER_EMAIL, $JOB_EXPERIENCE_ID]);
+
+        // $user = DB::select("SELECT * FROM API_USER_JOB_EXPERIENCE_DELETE($USER_NIF, '$USER_PASS', '$USER_EMAIL', '$JOB_EXPERIENCE_ID')"); 
 
         DB::commit();
 
