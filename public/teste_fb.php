a3107e0eb6640a89aba066167656c280ed350462
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2023-01-05 15:52:41 +0000

    Update teste_fb.php

--- public/teste_fb.php
+++ public/teste_fb.php
@@ -1,4 +1,6 @@
 <?php
+echo env('APP_URL');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -6,6 +8,7 @@ error_reporting(E_ALL);
 $host = "firebird:dbname=62.28.229.114:d:\one-key\swworking.gdb";
 $user = "SYSDBA";
 $pass = "60087.hs";
+
 
 $lokos = new PDO($host,$user,$pass);
 $stmt = $lokos->prepare("SELECT * FROM API_USER_JOB_EXPERIENCE_DELETE( '302755225' , 'dsdadsadasdsa' , 'ahmed@teste.pt' , 2)");
