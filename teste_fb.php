e1e2e53f42bc2a3afb189e4d8a470ada865e38dd
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2023-01-05 15:46:06 +0000

    Create teste_fb.php

---
+++ teste_fb.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = "firebird:dbname=62.28.229.114:d:\one-key\swworking.gdb";
+$user = "SYSDBA";
+$pass = "60087.hs";
+
+$lokos = new PDO($host,$user,$pass);
+$stmt = $lokos->prepare("SELECT * FROM API_USER_JOB_EXPERIENCE_DELETE( '302755225' , 'dsdadsadasdsa' , 'ahmed@teste.pt' , 2)");
+$stmt->execute();
+$dados = $stmt->fetchAll(PDO::FETCH_OBJ);
+$lokos->commit();
+print_r($dados);
+
+?>
