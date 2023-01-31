5cb775b464fa66beac615e1796c082c30e430e53
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2023-01-31 10:12:43 +0000

    Update OfferAPIController.php

--- app/Http/Controllers/API/OfferAPIController.php
+++ app/Http/Controllers/API/OfferAPIController.php
@@ -78,7 +78,7 @@ class OfferAPIController extends BaseController
         
         if (!empty($offer)){
 
-            dd($offer);
+            dd($offer->OFFER_ID);
             $allOffer = collect( [
                 'OFFER_ID' => $offer->OFFER_ID,
                 'RECRUITMENT_REFERENCE' => $offer->RECRUITMENT_REFERENCE,
