9817af01eb58f8e86da37a042aab1e112f6912b5
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2023-02-07 15:48:27 +0000

    Update UserAPIController.php

--- app/Http/Controllers/API/UserAPIController.php
+++ app/Http/Controllers/API/UserAPIController.php
@@ -1291,7 +1291,7 @@ class UserAPIController extends BaseController
 
             $all = collect( [
                 'CODIGO_PAIS' => $country->CODIGO_PAIS,
-                // 'NACIONALIDADE' => $this->convertUTF8($country->NACIONALIDADE),
+                'NACIONALIDADE' => $this->convertUTF8($country->NACIONALIDADE),
             ]);
 
             $countriesArray->push($all);
